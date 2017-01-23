381bfb6ddf6fe77aef2f5541d3dffd3fac4d0060
Gabriel Prado <gabriel.prado@walmart.com>
2017-01-23 18:26:57 -0200

    chore(package): add maven shade plugin to package app in uber-jar mode

--- delivery-app/src/main/java/com/guideapps/delivery/DeliveryApplication.java
+++ delivery-app/src/main/java/com/guideapps/delivery/DeliveryApplication.java
@@ -4,7 +4,7 @@ package com.guideapps.delivery;
  * Hello world!
  *
  */
-public class App 
+public class DeliveryApplication 
 {
     public static void main( String[] args )
     {
--- notifications-app/src/main/java/com/guideapps/notifications/NotificationsApplication.java
+++ notifications-app/src/main/java/com/guideapps/notifications/NotificationsApplication.java
@@ -4,7 +4,7 @@ package com.guideapps.notifications;
  * Hello world!
  *
  */
-public class App 
+public class NotificationsApplication 
 {
     public static void main( String[] args )
     {
