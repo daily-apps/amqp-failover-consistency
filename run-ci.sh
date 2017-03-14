ff80a1f662e8c0c76dba83485aab78dab1b8190b
Gabriel Prado <gabriel.prado@walmart.com>
2017-03-13 23:11:55 -0300

    refactor(scripts): better organization

--- run-ci.sh
+++ run-ci.sh
@@ -7,20 +7,12 @@ fi
 
 docker-compose -f docker-compose.base.yml -f docker-compose.ci.yml down
 
+# This scripts was extracted to other file to allow all apps to be independent and run on IntelliJ or otherwise
+./run-dev-deps.sh
+./run-cluster-config.sh
+
 # Build images
 docker-compose -f docker-compose.base.yml -f docker-compose.ci.yml build
-
-# After docker images builds, up base services
-docker-compose -f docker-compose.base.yml up -d
-docker-compose -f docker-compose.base.yml logs &
-sleep 15
-
-# Config cluster mode for slave
-docker-compose -f docker-compose.base.yml exec rabbitmq_slave /bin/bash -c "rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq_master; rabbitmqctl start_app"
-docker-compose -f docker-compose.base.yml exec rabbitmq_master rabbitmqctl set_policy ha-all "^payment\-" '{"ha-mode":"all", "ha-sync-mode":"automatic"}'
-
-# Enable plugin to migrate messagens between queues
-docker-compose -f docker-compose.base.yml exec rabbitmq_master rabbitmq-plugins enable rabbitmq_shovel rabbitmq_shovel_management
 
 # Run acceptance tests after docker images builds, and env steup
 # Better Practice: change all 4 acceptance tests for cucumber test, like:
--- run-dev.sh
+++ run-dev.sh
@@ -5,20 +5,14 @@ if ! which docker; then
     exit 1;
 fi
 
+docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml down
+
+# This scripts was extracted to other file to allow all apps to be independent and run on IntelliJ or otherwise
+./run-dev-deps.sh
+./run-cluster-config.sh
+
 # Build images
 docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml build
-
-# Up all cluster of rabbits, postgres, mail server and apps
-docker-compose -f docker-compose.base.yml up -d
-docker-compose -f docker-compose.base.yml logs &
-sleep 20
-
-# Config cluster mode for slave
-docker-compose -f docker-compose.base.yml exec rabbitmq_slave /bin/bash -c "rabbitmqctl stop_app; rabbitmqctl join_cluster rabbit@rabbitmq_master; rabbitmqctl start_app"
-docker-compose -f docker-compose.base.yml exec rabbitmq_master rabbitmqctl set_policy ha-all "^payment\-" '{"ha-mode":"all", "ha-sync-mode":"automatic"}'
-
-# Enable plugin to migrate messagens between queues
-docker-compose -f docker-compose.base.yml exec rabbitmq_master rabbitmq-plugins enable rabbitmq_shovel rabbitmq_shovel_management
 
 if [[ $# -eq 0 ]] ; then
     docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml up --no-recreate
